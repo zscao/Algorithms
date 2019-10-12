90bec9a96d9d664bfec2105ea4c70798a88cce70
Andy Cao <caozsn@gmail.com>
2019-10-12 17:53:37 +1100

    Towers of Hanio

---
+++ TowersOfHanio/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace TowersOfHanio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int level = 3;
+
+            if(args.Length >= 2)
+            {
+                var arg1 = args[0];
+                if(arg1 == "-l" || arg1 == "--level")
+                {
+                    if (int.TryParse(args[1], out int temp)) level = temp;
+                }
+            }
+
+
+            Console.WriteLine($"Towers of Hanio, Level: {level}");
+
+            var tower = new TowerOfHanio(level);
+            var solution = tower.Solve();
+            foreach (var s in solution)
+                Console.WriteLine(s);
+
+            Console.WriteLine($"Done. Total moves: {solution.Count()}");
+        }
+    }
+}
---
+++ TowersOfHanio/TowerOfHanio.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TowersOfHanio
+{
+    class TowerOfHanio
+    {
+        private string[] Pegs = { "A", "B", "C" };
+
+        public int Level
+        {
+            get;
+            private set;
+        }
+
+
+
+        public TowerOfHanio(int level)
+        {
+            if (level < 1) level = 1;
+            this.Level = level;
+        }
+
+        public IEnumerable<string> Solve()
+        {
+            return Move(this.Level, "A", "C");
+        }
+
+        private IEnumerable<string> Move(int n, string from, string to)
+        {
+            var result = new List<string>();
+
+            if (n == 1)
+            {
+                result.Add($"{n}: {from} -> {to}");
+                return result;
+            }
+
+            var swap = Pegs.First(x => x != from && x != to);
+            result.AddRange(Move(n - 1, from, swap));
+            result.Add($"{n}: {from} -> {to}");
+            result.AddRange(Move(n - 1, swap, to));
+
+            return result;
+        }
+    }
+}
